0554421aba936158746d87c9b0d76458b2f2b908
Wallace Rio <wallrio@gmail.com>
2018-04-19 06:13:03 -0300

    update

--- plugins/cifra/cifra.js
+++ plugins/cifra/cifra.js
@@ -1,2 +1,5 @@
 /* Vizoo - v1.0.0 - http://vizoo.online - Wallace Rio <wallrio@gmail.com> */
+/* Vizoo - v1.0.0 - http://vizoo.online - Wallace Rio <wallrio@gmail.com> */
+/* Vizoo - v1.0.0 - http://vizoo.online - Wallace Rio <wallrio@gmail.com> */
+/* Vizoo - v1.0.0 - http://vizoo.online - Wallace Rio <wallrio@gmail.com> */
 vizoo.plugins.cifra=function(){};
--- plugins/essentials/essentials.css
+++ plugins/essentials/essentials.css
@@ -1,1 +1,4 @@
 /* Vizoo - v1.0.0 - http://vizoo.online - Wallace Rio <wallrio@gmail.com> */
+/* Vizoo - v1.0.0 - http://vizoo.online - Wallace Rio <wallrio@gmail.com> */
+/* Vizoo - v1.0.0 - http://vizoo.online - Wallace Rio <wallrio@gmail.com> */
+/* Vizoo - v1.0.0 - http://vizoo.online - Wallace Rio <wallrio@gmail.com> */
