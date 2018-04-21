0ff2681c22439c901bea9efbac15fe4bbd9874e5
Wallace Rio <wallrio@gmail.com>
2018-04-21 03:41:59 -0300

    update

--- plugins/cifra/cifra.js
+++ plugins/cifra/cifra.js
@@ -1,2 +1,3 @@
 /* Vizoo - v1.0.0 - http://vizoo.online - Wallace Rio <wallrio@gmail.com> */
+/* Vizoo - v1.0.0 - http://vizoo.online - Wallace Rio <wallrio@gmail.com> */
 vizoo.plugins.cifra=function(){};
